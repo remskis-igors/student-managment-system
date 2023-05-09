008673265fa57f08d1ec9ff8784678869d778179
remskis-igors <122307773+remskis-igors@users.noreply.github.com>
2023-05-09 20:42:57 +0300

    first commit
    first commit

---
+++ student-managment-system/src/main/java/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import entity.Student;
+
+
+
+public interface StudentRepository extends JpaRepository <Student, Long>{
+
+}
